04032cfa33dec067ab4b7c50a1642df1a718a211
veringsek <veringsek@gmail.com>
2025-02-15 13:26:35 +0800

    -Extension

--- mkvmerge-bulk.ps1
+++ mkvmerge-bulk.ps1
@@ -1,23 +1,28 @@
-$script = $args[0]
-$output = $args[1]
-if (!($script)) {
-    $script = Get-Clipboard
+param (
+    [Alias("s")][string]$Script = $( Get-Clipboard ),
+    [Alias("o")][string]$Output = '!.cmd',
+    [Alias("e", "Ext")][string]$Extension = 'mkv'
+)
+# $Script = $args[0]
+# $Output = $args[1]
+if (!($Script)) {
+    $Script = Get-Clipboard
 }
-if (!($output)) {
-    $output = '!.cmd'
+if (!($Output)) {
+    $Output = '!.cmd'
 }
-$file = ([regex]'([a-zA-Z]:\\([^\\"]+\\)+)([^"]+)\.mkv').Matches($script)
+$file = ([regex]('([a-zA-Z]:\\([^\\"]+\\)+)([^"]+)\.' + $Extension)).Matches($Script)
 $path = $file[-1].Groups[1].Value
-$path = "$path$output".Replace('^', '')
+$path = "$path$Output".Replace('^', '')
 $filename = $file[-1].Groups[3].Value
 if (!($filename)) {
     Write-Error "File name not found."
-    Write-Error $script
+    Write-Error $Script
     exit
 }
 Write-Output "Detect file name $filename"
 Write-Output "Output to $path"
-$script = $script.Replace($filename, '%%~nf')
-$script = "chcp 65001`r`nfor %%f in (*.mkv) do $script`r`npause"
-Write-Output $script | Out-File -LiteralPath $path -Encoding utf8
+$Script = $Script.Replace($filename, '%%~nf')
+$Script = "chcp 65001`r`nfor %%f in (*.$Extension) do $Script`r`npause"
+Write-Output $Script | Out-File -LiteralPath $path -Encoding utf8
 Write-Output "Complete"
